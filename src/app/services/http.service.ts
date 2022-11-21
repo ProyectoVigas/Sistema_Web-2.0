27452c0d828d8756439ec054d630727be36aadb5
Martin98400 <67716758+Martin98400@users.noreply.github.com>
2022-11-21 12:36:19 -0700

    injectable

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { take } from 'rxjs/operators';
+import { NavigationExtras, Router } from "@angular/router";
+
+
+//primeng services
+import {MessageService} from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HttpService{
+    private apiUrl : string = "http://129.146.118.224:9001/";
+
+    constructor(private http: HttpClient,private message:MessageService, private router: Router) {}
+
+  public async post(method:string,payload:any = {}) : Promise<any>{
+    // if(!this.auth.validateToken()) this.router.navigate(['/unauthorized']);
+    return new Promise<any>((resolve,reject)=>{
+      const headers = new HttpHeaders();
+      headers.append("Content-Type","application/json");
+    //   headers.append("Authorization",`Bearer ${this.auth.token}`);
+      this.http.post(`${this.apiUrl}${method}`,payload,{headers}).pipe(
+        take(1),
+        )
+        .subscribe(res =>{
+        resolve(res); //PENDIENTE
+        },
+       error =>{
+        this.message.add({severity:"error",summary:"Error al consultar",detail:error.message,life:5000});
+        reject(error); //PENDIENTE
+       });
+    });
+  }
+
+  public async get(method:string) : Promise<any>{
+    // if(!this.auth.validateToken()) this.router.navigate(['/unauthorized']);
+    console.log(`${this.apiUrl}${method}`)
+    return new Promise<any>((resolve,reject)=>{
+      this.http
+      .get(`${this.apiUrl}${method}`)
+      .pipe(take(1))
+      .subscribe(
+        (res) =>{
+        resolve(res); //PENDIENTE
+        },
+       (error) =>{
+        this.message.add({severity:"error",summary:"Error al consultar",detail:error.message,life:5000});
+        reject(error); //PENDIENTE
+       });
+    });
+  }
+  
+  public async put(method:string,payload:any = {}) : Promise<any>{
+    // if(!this.auth.validateToken()) this.router.navigate(['/unauthorized']);
+    return new Promise<any>((resolve,reject)=>{
+      const headers = new HttpHeaders();
+      headers.append("Content-Type","application/json");
+    //   headers.append("Authorization",`Bearer ${this.auth.token}`);
+      this.http.put(`${this.apiUrl}${method}`,payload,{headers}).pipe(
+        take(1),
+        )
+        .subscribe(res =>{
+        resolve(res); //PENDIENTE
+        },
+       error =>{
+        this.message.add({severity:"error",summary:"Error al consultar",detail:error.message,life:5000});
+        reject(error); //PENDIENTE
+       });
+    });
+  }
+
+
+}
