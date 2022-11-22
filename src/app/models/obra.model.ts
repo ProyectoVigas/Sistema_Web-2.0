e8d72632a1ad0c2d11e8c20c3cdf88fe5827744d
Martin98400 <67716758+Martin98400@users.noreply.github.com>
2022-11-21 20:08:33 -0700

    registro obra(incompleto)

---
+++ src/app/models/obra.model.ts
@@ -0,0 +1,7 @@
+export default interface Obra {
+    ClvObra?: string;
+    NomObra?: string;
+    NumEmpleado?: string;
+    CantidadVigas?:string;
+    FechaObra?:Date;
+}
--- src/app/pages/obra/obra.component.ts
+++ src/app/pages/obra/obra.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+import { FormGroup, Validators, FormControl, FormBuilder} from '@angular/forms';
+import Obra from 'src/app/models/obra.model';
+
 
 @Component({
   selector: 'app-obra',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./obra.component.scss']
 })
 export class ObraComponent implements OnInit {
-
-  constructor() { }
+  // validacionesForm = new FormGroup({
+  //   NomObra: new FormControl(' ', Validators.compose([Validators.required,Validators.pattern(/^[\S][/,.,-,Á,É,Í,Ó,Ú,Ñ,á,é,í,ó,ú,ñ,a-zA-Z0-9 ]+$/)])),
+  //   NumEmpleado: new FormControl(' ', Validators.compose([Validators.required,Validators.pattern(/^\d+$/)])),
+  //   CantidadVidas: new FormControl(' ', Validators.compose([Validators.required,Validators.pattern(/^\d+$/)]))
+  // })
+  obra: Obra={
+    NomObra: '',
+    CantidadVigas: ''
+  }
+  nomobra: string='';
+  constructor(private http : HttpService ) { }
 
   ngOnInit(): void {
+  }
+
+  GuardarObra(){
+    // let obras=this.validacionesForm.value;
+    this.obra.NomObra = this.nomobra;
+    this.obra.NumEmpleado='9999';
+    this.obra.CantidadVigas= this.obra.CantidadVigas;
+    this.http.post('/Obra',this.obra).then((res)=> { })
   }
 
 }
